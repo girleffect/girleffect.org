2800ff306af460a3b21b7e44dec90e7a10e46374
Francesca Sargent <francesca.sargent@torchbox.com>
2017-10-11 12:52:04 +0000

    Initial commit of CarouselItemBlock and LinkBlock - no migrations in this commit

--- girleffect/utils/blocks.py
+++ girleffect/utils/blocks.py
@@ -24,7 +24,34 @@ class QuoteBlock(blocks.StructBlock):
         template = "blocks/quote_block.html"
 
 
+class LinkBlock(blocks.StructBlock):
+    external_link = blocks.URLBlock(required=False, label="External Link")
+    internal_link = blocks.PageChooserBlock(required=False, label="Internal Link")
+    link_text = blocks.CharBlock(max_length=255)
+
+    def get_context(self, value):
+        link = value['internal_link'].url if value.get('internal_link') else value['external_link']
+
+        context = super(LinkBlock, self).get_context(value)
+        context.update({
+            'link': link,
+        })
+        return context
+
+    class Meta:
+        template = "blocks/link_block.html"
+
+
+class CarouselItemBlock(blocks.StructBlock):
+    image = ImageChooserBlock()
+    label = blocks.CharBlock(max_length=255)
+    text = blocks.RichTextBlock(required=False)
+
+    class Meta:
+        template = "blocks/carousel_item_block.html"
+
 # Main streamfield block to be inherited by Pages
+
 
 class StoryBlock(blocks.StreamBlock):
     heading = blocks.CharBlock(classname="full title")
