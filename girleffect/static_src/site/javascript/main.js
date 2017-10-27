77afcc3713c5a461c93ccedeebd45a14ff1d9e0c
Chris Lawton <chris.lawton@torchbox.com>
2017-10-27 15:03:07 +0100

    linting after merge

--- girleffect/static_src/site/javascript/main.js
+++ girleffect/static_src/site/javascript/main.js
@@ -19,16 +19,16 @@ $(function() {
     $(Hamburger.selector()).each((index, el) => {
         new Hamburger($(el), openHeader, closeHeader);
     });
-  
+
     $('.js-carousel--full-width').owlCarousel({
-      items: 1,
-      nav: true,
-      dots: false,
-      navContainer: '.js-full-width-nav',
-      navText: ['&larr;','&rarr;'],
-      loop: $('.owl-carousel .carousel__item').length > 1 ? true : false
+        items: 1,
+        nav: true,
+        dots: false,
+        navContainer: '.js-full-width-nav',
+        navText: ['&larr;','&rarr;'],
+        loop: $('.owl-carousel .carousel__item').length > 1 ? true : false
     });
-  
+
     if ($('.owl-carousel .carousel__item').length === 1) {
         $('.carousel__nav').hide();
     }
