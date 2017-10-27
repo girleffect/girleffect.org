c5c750367744f5ff6ec9bce4bb66dfe9befc8926
Chris Lawton <chris.lawton@torchbox.com>
2017-10-27 09:31:08 +0100

    adding hamburger component

--- girleffect/static_src/site/javascript/main.js
+++ girleffect/static_src/site/javascript/main.js
@@ -1,16 +1,34 @@
 import $ from './globals';
 import 'owl.carousel';
 
-$(function() {
-    $('.js-carousel--full-width').owlCarousel({
-        items: 1,
-        nav: true,
-        dots: false,
-        navContainer: '.js-full-width-nav',
-        navText: ['&larr;','&rarr;'],
-        loop: $('.owl-carousel .carousel__item').length > 1 ? true : false
-    });
+import Hamburger from './../patterns/00-atoms/buttons/hamburger/hamburger.js';
 
+// Open the header callback
+function openHeader() {
+    $('.header').find('.header__row').slideDown(200);
+}
+
+// Close the header callback.
+function closeHeader() {
+    $('.header').find('.header__row').slideUp(200, () => {
+        this.node.next().removeAttr('style');
+    });
+}
+
+$(function() {
+    $(Hamburger.selector()).each((index, el) => {
+        new Hamburger($(el), openHeader, closeHeader);
+    });
+  
+    $('.js-carousel--full-width').owlCarousel({
+      items: 1,
+      nav: true,
+      dots: false,
+      navContainer: '.js-full-width-nav',
+      navText: ['&larr;','&rarr;'],
+      loop: $('.owl-carousel .carousel__item').length > 1 ? true : false
+    });
+  
     if ($('.owl-carousel .carousel__item').length === 1) {
         $('.carousel__nav').hide();
     }
