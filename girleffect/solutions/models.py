f37e7649289f638e94c27bcb3edc3130b809a1d0
Francesca Sargent <francesca.sargent@torchbox.com>
2017-10-31 14:54:05 +0000

    Removed unused import for PEP8 linting

--- girleffect/solutions/models.py
+++ girleffect/solutions/models.py
@@ -10,7 +10,7 @@ from girleffect.utils.models import (
     RelatedPage,
     SocialFields,
 )
-from girleffect.articles.models import ArticlePage, ArticlePageRelatedPage
+from girleffect.articles.models import ArticlePage
 
 from modelcluster.fields import ParentalKey
 
