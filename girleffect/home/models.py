2bfc4a41ddd1bf059704b2f1697a1d2abb09d970
Francesca Sargent <francesca.sargent@torchbox.com>
2017-11-20 17:18:32 +0000

    Adding streamfields, articles and CTA chooser

--- girleffect/home/models.py
+++ girleffect/home/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.utils.functional import cached_property
 
+from wagtail.wagtailadmin.edit_handlers import (
+    StreamFieldPanel
+)
+
+from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 
+from girleffect.articles.models import ArticlePage
 from girleffect.utils.models import (
     CallToActionSnippet,
     HeroVideoFields,
     SocialFields
 )
 
+from girleffect.utils.blocks import StoryBlock
+
 
 class HomePage(Page, HeroVideoFields, SocialFields):
     call_to_action = models.ForeignKey(CallToActionSnippet, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
+    body = StreamField(StoryBlock(), null=True)
+
+    @cached_property
+    def articles(self):
+        return ArticlePage.objects.all().live().public().order_by('-publication_date')[:3]
 
     content_panels = Page.content_panels + HeroVideoFields.content_panels + [
+        StreamFieldPanel('body'),
         SnippetChooserPanel('call_to_action'),
     ]
 
