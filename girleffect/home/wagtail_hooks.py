cbdd9502d6baacd05a18d51a4921e3fb0796964e
Francesca Sargent <francesca.sargent@torchbox.com>
2018-01-25 10:38:04 +0000

    [194] Adding justify hallo plugin to wysiwyg

--- girleffect/home/wagtail_hooks.py
+++ girleffect/home/wagtail_hooks.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Permission
 
+from wagtail.wagtailadmin.rich_text import HalloPlugin
 from wagtail.wagtailcore import hooks
 
 
@@ -9,3 +10,15 @@ def register_collection_permissions():
         content_type__app_label='wagtailcore',
         codename__in=['add_collection', 'change_collection', 'delete_collection']
     )
+
+
+@hooks.register('register_rich_text_features')
+def register_justify_feature(features):
+    features.register_editor_plugin(
+        'hallo', 'justify',
+        HalloPlugin(
+            name='hallojustify',
+            js=['/static/plugins/justify.js'],
+        )
+    )
+    features.default_features.append('justify')
