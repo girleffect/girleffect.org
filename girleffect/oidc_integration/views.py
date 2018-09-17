3a879c983da92d1d615f54218d721ee91960d8f7
cobusc <cobus.carstens@gmail.com>
2018-09-17 14:59:15 +0200

    Removed ununsed imports

--- girleffect/oidc_integration/views.py
+++ girleffect/oidc_integration/views.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.decorators import method_decorator
